5378238f7ea065bc899bf4b0ad1f371f9d6f307f
Ryan Braving <ryan.braving@gmail.com>
2019-02-17 23:15:26 -0700

    website is under development

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,40 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+// import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to Nutty Saffron Website.</p>
+    <p>This site is now under development and will be live soon.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    In the meantime you can find us on Instagram and Facebook{" "}
+    <b>@NuttySaffron</b> or through below links:
+    <br />
+    <a
+      href="https://www.facebook.com/nuttysaffron/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Facebook
+    </a>
+    <br />
+    <a
+      href="https://www.instagram.com/nuttysaffron/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Instagram
+    </a>
+    <br />
+    <br />
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
